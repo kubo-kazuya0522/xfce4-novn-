de993baf54d823ac5b9ebdde0c78857f01191833
久保和也 <241395598+kubo-kazuya0522@users.noreply.github.com>
2025-12-27 01:58:44 +0000

    kopiyou

---
+++ p.py
@@ -0,0 +1,22 @@
+@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl load-module module-null-sink sink_name=webrtc
+pactl set-default21
+-sink webrtc
+@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl set-default-sink webrtc
+@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl load-module module-null-sink sink_name=webrtc
+pa22
+ctl set-default-sink webrtc
+@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl set-default-sink webrtc
+@kubo-kazuya0522 ➜ ~/デスクトップ $ paplay /usr/share/sounds/freedesktop/stereo/complete.oga
+@kubo-kazuya0522 ➜ ~/デスクトップ $ paplay /usr/share/sounds/freedesktop/stereo/complete.oga
+@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl list short sink-inputs
+@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl load-module module-null-sink sink_name=webrtc
+23
+@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl set-default-sink webrtc
+@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl info | grep "Default Sink"
+@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl list short sinks
+1	webrtc	module-null-sink.c	s16le 2ch 44100Hz	IDLE
+2	webrtc.2	module-null-sink.c	s16le 2ch 44100Hz	IDLE
+3	webrtc.3	module-null-sink.c	s16le 2ch 44100Hz	IDLE
+4	webrtc.4	module-null-sink.c	s16le 2ch 44100Hz	IDLE
+@kubo-kazuya0522 ➜ ~/デスクトップ $ 
+
