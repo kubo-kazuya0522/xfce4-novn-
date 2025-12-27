1ff00f06a4754d945b315123206c88cfe0122223
久保和也 <241395598+kubo-kazuya0522@users.noreply.github.com>
2025-12-27 02:04:41 +0000

    mata

--- p.py
+++ p.py
@@ -1,22 +1,23 @@
+@kubo-kazuya0522 ➜ ~/デスクトップ $ pulseaudio -k
+sleep 2
+pulseaudio --start
+@kubo-kazuya0522 ➜ ~/デスクトップ $ sleep 2
+@kubo-kazuya0522 ➜ ~/デスクトップ $ pulseaudio --start
+@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl unload-module module-null-sink
+pactl unload-module module-null@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl unload-module module-null-sink
+pactl unlモジュールのアンロードに失敗: モジュール module-null-sink はロードされていません
+oad-modu@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl unload-module module-null-sink
+pactl unモジュールのアンロードに失敗: モジュール module-null-sink はロードされていません
+load-module module-n@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl unload-module module-null-sink
+モジュールのアンロードに失敗: モジュール module-null-sink はロードされていません
+@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl list short sinks
 @kubo-kazuya0522 ➜ ~/デスクトップ $ pactl load-module module-null-sink sink_name=webrtc
-pactl set-default21
--sink webrtc
-@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl set-default-sink webrtc
-@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl load-module module-null-sink sink_name=webrtc
-pa22
-ctl set-default-sink webrtc
-@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl set-default-sink webrtc
-@kubo-kazuya0522 ➜ ~/デスクトップ $ paplay /usr/share/sounds/freedesktop/stereo/complete.oga
-@kubo-kazuya0522 ➜ ~/デスクトップ $ paplay /usr/share/sounds/freedesktop/stereo/complete.oga
-@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl list short sink-inputs
-@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl load-module module-null-sink sink_name=webrtc
-23
+19
 @kubo-kazuya0522 ➜ ~/デスクトップ $ pactl set-default-sink webrtc
 @kubo-kazuya0522 ➜ ~/デスクトップ $ pactl info | grep "Default Sink"
 @kubo-kazuya0522 ➜ ~/デスクトップ $ pactl list short sinks
 1	webrtc	module-null-sink.c	s16le 2ch 44100Hz	IDLE
-2	webrtc.2	module-null-sink.c	s16le 2ch 44100Hz	IDLE
-3	webrtc.3	module-null-sink.c	s16le 2ch 44100Hz	IDLE
-4	webrtc.4	module-null-sink.c	s16le 2ch 44100Hz	IDLE
+@kubo-kazuya0522 ➜ ~/デスクトップ $ paplay --device=webrtc /usr/share/sounds/freedesktop/stereo/complete.oga
+@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl list short sink-inputs
 @kubo-kazuya0522 ➜ ~/デスクトップ $ 
 
