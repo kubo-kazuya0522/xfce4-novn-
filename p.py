e8e3f7617c3760d7c20a3213d5845d59ccc40abc
久保和也 <241395598+kubo-kazuya0522@users.noreply.github.com>
2025-12-27 02:25:09 +0000

    、matamata

--- p.py
+++ p.py
@@ -1,23 +1,6 @@
-@kubo-kazuya0522 ➜ ~/デスクトップ $ pulseaudio -k
-sleep 2
-pulseaudio --start
-@kubo-kazuya0522 ➜ ~/デスクトップ $ sleep 2
-@kubo-kazuya0522 ➜ ~/デスクトップ $ pulseaudio --start
-@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl unload-module module-null-sink
-pactl unload-module module-null@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl unload-module module-null-sink
-pactl unlモジュールのアンロードに失敗: モジュール module-null-sink はロードされていません
-oad-modu@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl unload-module module-null-sink
-pactl unモジュールのアンロードに失敗: モジュール module-null-sink はロードされていません
-load-module module-n@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl unload-module module-null-sink
-モジュールのアンロードに失敗: モジュール module-null-sink はロードされていません
-@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl list short sinks
-@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl load-module module-null-sink sink_name=webrtc
-19
-@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl set-default-sink webrtc
-@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl info | grep "Default Sink"
-@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl list short sinks
-1	webrtc	module-null-sink.c	s16le 2ch 44100Hz	IDLE
-@kubo-kazuya0522 ➜ ~/デスクトップ $ paplay --device=webrtc /usr/share/sounds/freedesktop/stereo/complete.oga
-@kubo-kazuya0522 ➜ ~/デスクトップ $ pactl list short sink-inputs
-@kubo-kazuya0522 ➜ ~/デスクトップ $ 
-
+@kubo-kazuya0522 ➜ ~/デスクトップ $ gst-launch-1.0 -v \
+>   pulsesrc device=webrtc.monitor ! \
+>   audioconvert ! audioresample ! \
+>   opusenc ! rtpopuspay ! \
+>   webrtcbin name=webrtc stun-server=stun://stun.l.google.com:19302
+WARNING: erroneous pipeline: rtpopuspay0 を webrtc へリンクできません
